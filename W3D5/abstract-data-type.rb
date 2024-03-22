a6c180a6bfaa3753701f4f1fec997b7a6ae82790
Jaspreet Singh <99jasingh@gmail.com>
2024-03-22 01:55:48 -0400

    Finish exercise-3

--- W3D5/abstract-data-type.rb
+++ W3D5/abstract-data-type.rb
@@ -43,3 +43,48 @@ class Queue
   attr_reader :queue
 end
 
+#Exercise 3
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    if (pair_index = @map.find { |pair| pair[0] == key })
+      @map[pair_index][1] = value
+    else
+      @map.push([key, value])
+    end
+    value
+  end
+
+  def get(key)
+    pair = @map.find { |pair| pair[0] == key }
+    if pair
+      return pair[1]
+    else
+      return nil
+    end
+  end
+
+  def delete(key)
+    @map.delete_if { |pair| pair[0] == key }
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  def deep_dup(arr)
+    arr.map do |el|
+      if el.is_a?(Array)
+        deep_dup(el)
+      else
+        el
+      end
+    end
+  end
+
+  attr_reader :map
+end
