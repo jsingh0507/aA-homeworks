6d44eb872b084445a47efd20cb7f9ca945e0d479
Jaspreet Singh <99jasingh@gmail.com>
2024-03-24 23:57:50 -0400

    commit hw

---
+++ W4D1/graph.rb
@@ -0,0 +1,29 @@
+require 'set'
+
+class GraphNode
+  def initialize(val)
+    self.val = val
+    self.neighbors = []
+  end
+
+  def add_neighbor(node)
+    self.neighbors << node
+  end
+  attr_accessor :val, :neighbors
+end
+
+def bfs(starting_node, target_value)
+  queue = [starting_node]
+  visited = Set.new()
+
+  until queue.empty?
+    node = queue.shift
+    unless visited.include?(node)
+      return node.val if node.val == target_value
+      visited.add(node)
+      queue += node.neighbors
+    end
+  end
+  
+  return nil
+end
