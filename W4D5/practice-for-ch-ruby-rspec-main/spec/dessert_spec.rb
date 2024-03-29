313681db52958c52455b78e4cba9eb146b3e84b6
Jaspreet Singh <99jasingh@gmail.com>
2024-03-28 23:39:26 -0400

    completed W4D5 hw

--- W4D5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
+++ W4D5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
@@ -7,36 +7,67 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
+  let(:oat) { Dessert.new("oat", 10, chef) }
 
   describe "#initialize" do
-    it "sets a type"
-
-    it "sets a quantity"
-
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "sets a type" do
+      expect(oat.type).to eq("oat")
+    end
+    it "sets a quantity" do
+      expect(oat.quantity).to eq(10)
+    end
+    it "starts ingredients as an empty array" do
+      expect(oat.ingredients).to be_empty
+    end
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "tons", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      oat.add_ingredient("chocolate")
+      expect(oat.ingredients).to include("chocolate")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate", "flour", "egg", "sugar", "butter"]
+
+      ingredients.each do |ingredient|
+        oat.add_ingredient(ingredient)
+      end
+
+      expect(oat.ingredients).to eq(ingredients)
+      oat.mix!
+      expect(oat.ingredients).not_to eq(ingredients)
+      expect(oat.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      oat.eat(2)
+      expect(oat.quantity).to eq(8)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect {oat.eat(15)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Jass the Great Baker")
+      expect(oat.serve).to eq("Chef Jass the Great Baker has made 10 oats!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(oat)
+      oat.make_more
+    end
   end
 end
