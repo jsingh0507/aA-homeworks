d2533994610f259d07fbce2123f27ab128b20af5
Jaspreet Singh <99jasingh@gmail.com>
2024-04-13 19:07:50 -0400

    finished hw

--- W7D1/hwproject/app/models/user.rb
+++ W7D1/hwproject/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_reader :password
     validates :username, :session_token, presence: true
     validates :password_digest, presence: { message: "Password can't be blank" }
     validates :password, length: { minimum: 6 }, allow_nil: true
