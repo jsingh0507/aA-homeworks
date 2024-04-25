06820a39e760225561f6e39cfc3755f72f26be8f
Jaspreet Singh <99jasingh@gmail.com>
2024-04-25 01:17:21 -0400

    finished hw1

--- W8D4/practice-for-ch-js-intro-to-canvas-main/canvas-practice.js
+++ W8D4/practice-for-ch-js-intro-to-canvas-main/canvas-practice.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Your code here
+  const canvasEl = document.getElementById("myCanvas");
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+
+  const ctx = canvasEl.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(100, 100, 50, 0, 2*Math.PI, true);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.fillStyle = 'green';
+  ctx.fill(); 
+
+  ctx.beginPath();
+  ctx.moveTo(400, 100);
+  ctx.lineTo(300, 200); 
+  ctx.lineTo(500, 200);
+  ctx.closePath();
+  ctx.fillStyle = 'orange';
+  ctx.fill();
+
 });
