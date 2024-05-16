ed367ab1e8973a72f393379fe17638569d00a789
Jaspreet Singh <99jasingh@gmail.com>
2024-05-15 23:41:20 -0400

    gift done

--- W11D4/practice-for-ch-react-jbuilder-long-practice-main/config/routes.rb
+++ W11D4/practice-for-ch-react-jbuilder-long-practice-main/config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    resources :guests, only: [:index, :show] do
+      resources :gifts, only: [:index, :show], shallow: true 
+    end
+    resources :parties, only: [:index, :show]
+  end
 end
